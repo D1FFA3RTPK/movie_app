d2c3e221ca74455e83f8eadee41385afd219565f
D1FFA3RTPK <mmamm3628@naver.com>
2020-10-25 19:57:52 +0900

    Updates

---
+++ precache-manifest.dcd8329b3d19887295e806b98e591192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd8a22c7503a39adca14ef0f5ef812b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9fea9322dcacc193f878",
+    "url": "/movie_app/static/css/main.d6fb4968.chunk.css"
+  },
+  {
+    "revision": "39f75bacc7e29a4ac856",
+    "url": "/movie_app/static/js/2.e533ec29.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/movie_app/static/js/2.e533ec29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fea9322dcacc193f878",
+    "url": "/movie_app/static/js/main.1ccb8efe.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
